6a970a093e76edd0893b8d89d695f631ce8ba825
Alex Villanueva <138938590+liminalpup@users.noreply.github.com>
2024-07-05 01:11:10 -0400

    first client commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState, useEffect } from "react";
+import Axios from 'axios';
 function App() {
+  const [foodName, setFoodName] = useState("");
+  const [days, setDays] = useState(0);
+
+  const [foodList, setFoodList] = useState([]);
+  useEffect(() => {
+    Axios.get("http://localhost:3001/read").then((res) => {
+      setFoodList(res.data);
+    })
+  }, [])
+
+  const addToList = () => {
+    Axios.post("http://localhost:3001/insert", {
+      foodName: foodName,
+      days: days,
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CRUD App with MERN</h1>
+      <label>Food Name:</label>
+      <input type="text" onChange={(event) => {
+        setFoodName(event.target.value);
+      }} />
+
+      <label>Days Since You Ate It:</label>
+      <input type="number" onChange={(event) => {
+        setDays(event.target.value);
+      }} />
+      <button onClick={addToList}>Add To List</button>
+      <h1>Food List:</h1>
+      {foodList.map((val, key) => {
+        return <div key={key}>
+          <h1>{val.foodName}</h1>
+          <h1>{val.daysSinceIAte}</h1>
+        </div>
+      })}
     </div>
   );
 }
